b4dce34a2b94cbb17bf4b0b02f87dc3b41d9331c
tokebe <43009413+tokebe@users.noreply.github.com>
2021-04-22 12:31:21 -0400

    move stateCodes to own file

--- webpage/viz.js
+++ webpage/viz.js
@@ -1,68 +1,6 @@
 const width = 960;
 const height = 500;
 
-stateCodes = {
-  Alabama: "AL",
-  Alaska: "AK",
-  "American Samoa": "AS",
-  Arizona: "AZ",
-  Arkansas: "AR",
-  California: "CA",
-  Colorado: "CO",
-  Connecticut: "CT",
-  Delaware: "DE",
-  "District Of Columbia": "DC",
-  "Federated States Of Micronesia": "FM",
-  Florida: "FL",
-  Georgia: "GA",
-  Guam: "GU",
-  Hawaii: "HI",
-  Idaho: "ID",
-  Illinois: "IL",
-  Indiana: "IN",
-  Iowa: "IA",
-  Kansas: "KS",
-  Kentucky: "KY",
-  Louisiana: "LA",
-  Maine: "ME",
-  "Marshall Islands": "MH",
-  Maryland: "MD",
-  Massachusetts: "MA",
-  Michigan: "MI",
-  Minnesota: "MN",
-  Mississippi: "MS",
-  Missouri: "MO",
-  Montana: "MT",
-  Nebraska: "NE",
-  Nevada: "NV",
-  "New Hampshire": "NH",
-  "New Jersey": "NJ",
-  "New Mexico": "NM",
-  "New York": "NY",
-  "North Carolina": "NC",
-  "North Dakota": "ND",
-  "Northern Mariana Islands": "MP",
-  Ohio: "OH",
-  Oklahoma: "OK",
-  Oregon: "OR",
-  Palau: "PW",
-  Pennsylvania: "PA",
-  "Puerto Rico": "PR",
-  "Rhode Island": "RI",
-  "South Carolina": "SC",
-  "South Dakota": "SD",
-  Tennessee: "TN",
-  Texas: "TX",
-  Utah: "UT",
-  Vermont: "VT",
-  "Virgin Islands": "VI",
-  Virginia: "VA",
-  Washington: "WA",
-  "West Virginia": "WV",
-  Wisconsin: "WI",
-  Wyoming: "WY",
-};
-
 const projection = d3
   .geoAlbersUsa()
   .scale(1000)
@@ -87,26 +25,27 @@ function getProjectedCoord(d, i) {
   return projection([d.lng, d.lat])[i];
 }
 
-function getStateSightings(d, data) {
-  const stateCode = stateCodes[d.properties.name];
-  // console.log(stateCode);
-  const val = stateCode ? data[1].filter((d) => d.State === stateCode) : [];
-
-  return val.length > 0 ? val[0].Sightings : undefined;
-}
-
 Promise.all([
   d3.json("data/states-10m.json"),
   d3.csv("data/sighting-per-state.csv"),
+  d3.json("data/stateCodes.json"),
 ]).then((data) => {
-  console.log(data[1]);
+  function getStateSightings(d, data) {
+    const stateCode = stateCodes[d.properties.name];
+    // console.log(stateCode);
+    const val = stateCode ? data[1].filter((d) => d.State === stateCode) : [];
+
+    return val.length > 0 ? val[0].Sightings : undefined;
+  }
+
+  console.log(data);
+  const stateCodes = data[2];
   const colorScale = d3
     .scaleSequential()
     .domain(d3.extent(data[1], (d) => parseFloat(d.Sightings)))
     .interpolator(d3.interpolateViridis);
 
   console.log();
-  // console.log(data);
   svg
     .append("g")
     .selectAll("path")
